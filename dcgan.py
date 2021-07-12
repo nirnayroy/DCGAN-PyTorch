209d4c3c8a396a1d2735950bba6e6eecc0ac40b6
nirnayroy <32942494+nirnayroy@users.noreply.github.com>
2021-07-12 20:03:29 +0530

    Update dcgan.py

--- dcgan.py
+++ dcgan.py
@@ -134,6 +134,7 @@ if __name__ == "__main__":
 
     with torch.no_grad():
         generated_img = netG(masked_img.reshape(-1, 1, 140, 280).float()).detach()
+        print('generated image shape', generated_img.shape)
         generated_img = (1.-mask)*generated_img + masked_img
     print(generated_img.shape)
 
