435a0c61f219cdcd4ddfadeb187004a6e8368342
nirnayroy <32942494+nirnayroy@users.noreply.github.com>
2021-07-13 15:05:07 +0530

    Update train.py

--- train.py
+++ train.py
@@ -175,7 +175,7 @@ for epoch in range(params['nepochs']):
                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
         with torch.no_grad():
             fake_data = netG(masked_data).detach().cpu().numpy()
-            fake_data = (1.-mask)*fake_data + masked_data
+            fake_data = (1.-mask.cpu().numpy())*fake_data + masked_data.cpu().numpy()
             plt.imshow(fake_data[0,0,:,:])
             plt.savefig('op_epoch_{}'.format(epoch))
         torch.save({
